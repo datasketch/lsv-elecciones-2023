0dd61b9bf258aa495f025cd3a4713a95849616ef
David Daza <03dazal@gmail.com>
2022-06-02 15:25:46 -0500

    Update slider images

--- src/components/presidential/Carousel.jsx
+++ src/components/presidential/Carousel.jsx
@@ -7,52 +7,55 @@ function Carousel() {
     <div className="max-w-lg mx-auto py-12 overflow-hidden">
       <Slider dots>
         <div className="aspect-w-1 aspect-h-1">
-          <img src="/carousel/1.jpg" alt="" />
+          <img src="/carousel/1.png" alt="" />
         </div>
         <div className="aspect-w-1 aspect-h-1">
-          <img src="/carousel/2.jpg" alt="" />
+          <img src="/carousel/2.png" alt="" />
         </div>
         <div className="aspect-w-1 aspect-h-1">
-          <img src="/carousel/3.jpg" alt="" />
+          <img src="/carousel/3.png" alt="" />
         </div>
         <div className="aspect-w-1 aspect-h-1">
-          <img src="/carousel/4.jpg" alt="" />
+          <img src="/carousel/4.png" alt="" />
         </div>
         <div className="aspect-w-1 aspect-h-1">
-          <img src="/carousel/5.jpg" alt="" />
+          <img src="/carousel/5.png" alt="" />
         </div>
         <div className="aspect-w-1 aspect-h-1">
-          <img src="/carousel/6.jpg" alt="" />
+          <img src="/carousel/6.png" alt="" />
         </div>
         <div className="aspect-w-1 aspect-h-1">
-          <img src="/carousel/7.jpg" alt="" />
+          <img src="/carousel/7.png" alt="" />
         </div>
         <div className="aspect-w-1 aspect-h-1">
-          <img src="/carousel/8.jpg" alt="" />
+          <img src="/carousel/8.png" alt="" />
         </div>
         <div className="aspect-w-1 aspect-h-1">
-          <img src="/carousel/9.jpg" alt="" />
+          <img src="/carousel/9.png" alt="" />
         </div>
         <div className="aspect-w-1 aspect-h-1">
-          <img src="/carousel/10.jpg" alt="" />
+          <img src="/carousel/10.png" alt="" />
         </div>
         <div className="aspect-w-1 aspect-h-1">
-          <img src="/carousel/11.jpg" alt="" />
+          <img src="/carousel/11.png" alt="" />
         </div>
         <div className="aspect-w-1 aspect-h-1">
-          <img src="/carousel/12.jpg" alt="" />
+          <img src="/carousel/12.png" alt="" />
         </div>
         <div className="aspect-w-1 aspect-h-1">
-          <img src="/carousel/13.jpg" alt="" />
+          <img src="/carousel/13.png" alt="" />
         </div>
         <div className="aspect-w-1 aspect-h-1">
-          <img src="/carousel/14.jpg" alt="" />
+          <img src="/carousel/14.png" alt="" />
         </div>
         <div className="aspect-w-1 aspect-h-1">
-          <img src="/carousel/15.jpg" alt="" />
+          <img src="/carousel/15.png" alt="" />
         </div>
         <div className="aspect-w-1 aspect-h-1">
-          <img src="/carousel/16.jpg" alt="" />
+          <img src="/carousel/16.png" alt="" />
+        </div>
+        <div className="aspect-w-1 aspect-h-1">
+          <img src="/carousel/17.png" alt="" />
         </div>
       </Slider>
     </div>
